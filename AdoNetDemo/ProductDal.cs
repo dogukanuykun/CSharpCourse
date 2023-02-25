423820cbd30c015335479a7b9064f4cf7fa86f4d
uykun <uykun@DESKTOP-5MH8OB3>
2023-02-25 14:23:07 +0300

    Add project files.

---
+++ AdoNetDemo/ProductDal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDemo
+{
+    public class ProductDal
+    {
+        public DataTable GelAll()
+        {
+            SqlConnection connection = new SqlConnection(@"server=(localdb)\mssqllocaldb;initial catalog=ETrade; integrated security=true");
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            SqlCommand command = new SqlCommand("SELECT * FROM PRODUCTS", connection);
+            
+            SqlDataReader reader = command.ExecuteReader();
+
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+            reader.Close();
+            connection.Close();
+            return dataTable;
+        }
+    }
+}
---
+++ Exceptions/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HandleException(() =>
+            {
+                Find();
+            });
+        }
+
+        private static void HandleException(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        private static void Find()
+        {
+            List<string> students = new List<string> { "Doğukan", "Tuna", "Ömer"};
+            if (!students.Contains("Ahmet"))
+            {
+                throw new RecordNotFoundException("Record not Found!");
+
+            }
+            else
+            {
+                Console.WriteLine("REcord Found!");
+            }
+        }
+    }
+}
