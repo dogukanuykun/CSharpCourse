2470f533df54ef92388d1f160d3f482205972c9d
dogukanuykun <uykundogukan@gmail.com>
2023-02-25 14:35:22 +0300

    ProductDal configurations without DataTable object.

--- AdoNetDemo/ProductDal.cs
+++ AdoNetDemo/ProductDal.cs
@@ -10,7 +10,7 @@ namespace AdoNetDemo
 {
     public class ProductDal
     {
-        public DataTable GelAll()
+        public List<Product> GelAll()
         {
             SqlConnection connection = new SqlConnection(@"server=(localdb)\mssqllocaldb;initial catalog=ETrade; integrated security=true");
             if (connection.State == System.Data.ConnectionState.Closed)
@@ -21,11 +21,22 @@ namespace AdoNetDemo
             
             SqlDataReader reader = command.ExecuteReader();
 
-            DataTable dataTable = new DataTable();
-            dataTable.Load(reader);
+            List<Product> products= new List<Product>();
+            while (reader.Read())
+            {
+                Product product = new Product
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Name = Convert.ToString(reader["Name"]),
+                    StockAmount = Convert.ToInt32(reader["StockAmount"]),
+                    UnitPrice = Convert.ToDecimal(reader["UnitPrice"])
+                };
+                products.Add(product);
+            }
+
             reader.Close();
             connection.Close();
-            return dataTable;
+            return products;
         }
     }
 }
