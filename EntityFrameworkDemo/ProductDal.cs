46f69ecadb052f4df63d353d8dc659975ad6f698
dogukanuykun <uykundogukan@gmail.com>
2023-02-27 13:56:39 +0300

    Filter products with unitPrice.

--- EntityFrameworkDemo/ProductDal.cs
+++ EntityFrameworkDemo/ProductDal.cs
@@ -19,11 +19,35 @@ namespace EntityFrameworkDemo
             }
         }
 
+        public Product GetById(int id)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                return context.Products.FirstOrDefault(p => p.Id == id);
+            }
+        }
+
         public List<Product> GetByName(string key)
         {
             using (ETradeContext context = new ETradeContext())
             {
                 return context.Products.Where(p => p.Name.Contains(key)).ToList();
+            }
+        }
+
+        public List<Product> GetByUnitPrice(decimal price)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                return context.Products.Where(p => p.UnitPrice >= price).ToList();
+            }
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                return context.Products.Where(p => p.UnitPrice >= min && p.UnitPrice <= max).ToList();
             }
         }
 
