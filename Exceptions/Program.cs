b0c551b4ea8599d8f59433bee8057b1af1c0db1c
dogukanuykun <uykundogukan@gmail.com>
2023-03-02 02:11:02 +0300

    Working with Func & delegates.

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Remoting.Channels;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Exceptions
@@ -10,6 +11,33 @@ namespace Exceptions
     internal class Program
     {
         static void Main(string[] args)
+        {
+
+            Func<int, int, int> add = Sum;
+            
+            Console.WriteLine(add(3,5));
+
+            Func<int> getRandomNumber = delegate ()
+            {
+                Random random = new Random();
+                return random.Next(1, 100);
+            };
+
+            Func<int> getRandomNumber2 = () => new Random().Next(1, 100);
+
+            Console.WriteLine(getRandomNumber());
+            Thread.Sleep(1000);
+            Console.WriteLine(getRandomNumber2());
+            //Console.WriteLine(Sum(2, 3));
+
+        }
+
+        static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        private static void ActionDemo()
         {
             HandleException(() =>
             {
@@ -30,7 +58,7 @@ namespace Exceptions
             }
         }
 
-        private static void Find()
+        private static void Find() //Basic function for handling errors
         {
             List<string> students = new List<string> { "Doğukan", "Tuna", "Ömer"};
             if (!students.Contains("Ahmet"))
