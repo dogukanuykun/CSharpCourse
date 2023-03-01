7993d0c65e894b26d277503e28ee53c92297dcd4
dogukanuykun <uykundogukan@gmail.com>
2023-03-01 16:39:09 +0300

    Reflaction example 1

---
+++ Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+
+    interface IProduct
+    {
+        List<Product> GetAll();
+        Product Get(int id);
+    }
+
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DortIslem dortIslem = new DortIslem(2,3);
+            //Console.WriteLine(dortIslem.Topla2());
+            //Console.WriteLine(dortIslem.Topla(3, 2));
+            var type = typeof(Calc);
+
+            Calc calc = (Calc)Activator.CreateInstance(type, 2 ,3);
+            Console.WriteLine(calc.Sum(3,5));
+            Console.ReadLine();
+            
+        }
+    }
+
+    public class Calc
+    {
+
+        private int _num1, _num2;
+
+        public Calc(int num1, int num2)
+        {
+            _num1 = num1;
+            _num2 = num2;
+        }
+
+
+        public int Sum (int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public int Sum2()
+        {
+            return _num1 + _num2;
+        }
+
+        public int Multiply2()
+        {
+            return _num1 * _num2;
+        }
+
+
+
+    }
+
+}
